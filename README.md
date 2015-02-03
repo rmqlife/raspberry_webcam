e7368a0cf66734e9e07d12481a00950e04cdc6f2
rmqlife <rmqlife@gmail.com>
2015-02-03 21:04:16 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# raspberry pi webcam setting
+this is a simple script for raspberry pi and Logitech C170, which solved the over explosure problem.
+
+## USE
+`apt-get install fswebcam`
+`chmod +x webcam.sh`
+`./webcam.sh`
+
+
---
+++ webcam.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# get time stamp filename
+camname=~/webcam/pics/$(date +"%Y-%m-%d-%H-%M-%S").jpg
+echo $camname
+
+# fswebcam -p YUYV is for logitech C170 
+sudo fswebcam -r 640x480 -p YUYV -S 60  $camname
+
+# send this to remote image
+# scp $camname rmqlife@www.rmqlife.com:~/pics/cam.jpg
+rsync $camname rmqlife@www.rmqlife.com:~/pics/cam.jpg
+
